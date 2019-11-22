c296d84d262ecd3409d085a27fde18f6a5871b5e
Joseph Tran <ptran3@nd.edu>
2019-11-21 23:28:03 -0500

    Created map borders

--- snake_game.cpp
+++ snake_game.cpp
@@ -1,8 +1,48 @@
 #include<iostream>
+//#include<windows.h>
 using namespace std;
 
+int mapx = 40; //map width
+int mapy = 20; //map height
+
+void makeMap();
+void input();
+void move();
+
 int main(){
-
-
+	makeMap();
 	return 0;
 }
+
+// Create a map
+void makeMap(){
+	//system("cls");
+	//create walls
+	for (int i = 0; i <= mapy+1; ++i){
+		for (int j = 0; j <= mapx +1; ++j){
+			if (i == 0 || i == mapy+1){
+				cout << "-";
+			}
+			else if (i > 0 && i < mapy+1){
+				if (j == 0 || j == mapx+1){
+					cout << "|";
+				}
+				else {
+					cout << " ";
+				}
+			}
+		}
+		cout << endl;
+	}
+
+}
+
+// Take input from keyboard
+void input(){
+
+}
+
+// Logic for moving
+void move(){
+
+} 
