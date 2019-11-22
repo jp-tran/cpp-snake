d7d589292910c0ddc959d2219c6ce29974adf395
Stephen Koch <skoch@student00.cse.nd.edu>
2019-11-21 21:50:46 -0500

    test commit

--- snake_game.cpp
+++ snake_game.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main(){
-
+	// Graphics Window 
+	// this section will print the snake object to the screen 
+	// sample code here 
+	// yada yada yada 
 
 	return 0;
 }
