8b9fe3acb363a73658993918d9162b7693c2c63c
Stephen Koch <skoch@student11.cse.nd.edu>
2019-12-08 15:17:08 -0500

    added hit detection, changed to arrays

--- snake_game.cpp
+++ snake_game.cpp
@@ -1,34 +1,70 @@
 #include<iostream>
 #include<stdlib.h>
-#include<curses.h> 
+#include<curses.h>
+#include<unistd.h> 
 
 using namespace std;
 
 
 int mapx = 40; //map width
-int mapy = 20; //map height
+int mapy = 20; //map height 
 
 void makeMap();
 void input();
 void move();
-
+void printEntity(int, int[], int[]); 
+void entityControl(int[], int[]); 
 int main(){	
 	initscr(); 
 	cbreak(); 	
 	noecho(); 
 	clear(); 
-	
-	int maxlines, maxcols;
-	maxlines = LINES - 1; 
-	maxcols = COLS -1; 
-	mvaddch(maxlines/3, maxcols/3, 't'); 
-	
-	getch(); 
+	curs_set(0); 
+	int pos[2], screen[2];
+	// array containing [0] = # of lines [1] = # of cols 
+	screen[0] = LINES - 1; 
+	screen[1] = COLS -1; 
+	// array containing [y pos, xpos] 
+	pos[0] = screen[0] / 2; 
+	pos[1] = screen[1] /2; 
+	// print initial entity
+	printEntity(1, pos, screen); 
+	entityControl(pos, screen);  
 	endwin(); 
 
 	return 0;
 }
 
+void entityControl(int pos[2], int screen[2]) { 
+	int ch = getch(); 
+	while(ch != 101) {
+		ch = getch();  
+		if (ch == 119) { // w (up)
+		pos[0] = pos[0] - 1;  
+		printEntity(1, pos, screen); 
+		} 
+		else if (ch == 97) { // a (left)
+		pos[1] = pos[1] - 1;  
+		printEntity(1, pos, screen); 
+		}
+		else if (ch == 115) { // s (down) 
+		pos[0] = pos[0] + 1;  
+		printEntity(1, pos, screen);
+		} 
+		else if (ch == 100) { // d (right) 
+		pos[1] = pos[1] + 1; 
+		printEntity(1, pos, screen); 
+		}
+	}
+}
+void printEntity(int length, int pos[2], int screen[2]){ 
+	if ((pos[0] > 0)&&(pos[0] < screen[0])&&(pos[1] > 0)&&(pos[1] < screen[1])) {
+		clear();
+		mvaddch(pos[0], pos[1], 'x');
+		refresh;   
+	} 
+	 			
+}
 // Create a map
 void makeMap(){
 	system("clear");
