2e0a84c468fed634cdbbaf589a57bcaa37dc75e4
Joseph Tran <ptran3@nd.edu>
2019-12-08 17:30:32 -0500

    Added map borders and food

--- snake_game.cpp
+++ snake_game.cpp
@@ -2,28 +2,25 @@
 #include<stdlib.h>
 #include<curses.h>
 #include<unistd.h> 
+#include<time.h>
 
 using namespace std;
 
-
-int mapx = 40; //map width
-int mapy = 20; //map height 
-
-void makeMap();
-void input();
+void makeMap(int[]);
+int* spawnFood(int[]);
 void move();
 void printEntity(int, int[], int[]); 
-void entityControl(int[], int[]); 
+void entityControl(int[], int[], int[]); 
 int main(){	
-	initscr(); 
+	initscr();
 	cbreak(); 	
 	noecho(); 
 	clear(); 
 	curs_set(0); 
 	int pos[3], screen[2];
 	// array containing [0] = # of lines [1] = # of cols 
-	screen[0] = LINES - 1; 
-	screen[1] = COLS -1; 
+	screen[0] = LINES - 40; 
+	screen[1] = COLS - 70; 
 	// array containing [y pos, xpos, directoion], where 
 	// direction = 1 - facing right 
 	// direction = 2 - facing left 
@@ -32,15 +29,19 @@ int main(){
 	pos[0] = screen[0] / 2; 
 	pos[1] = screen[1] /2; 
 	pos[2] = 1; 
-	// print initial entity
-	printEntity(1, pos, screen); 
-	entityControl(pos, screen);  
+	// print initial entity, map, and food
+	makeMap(screen);
+	printEntity(1, pos, screen);
+	int* foodPos = spawnFood(screen); //get location of food
+	mvaddch(foodPos[0], foodPos[1], 'O');
+	refresh;
+	entityControl(pos, screen, foodPos);  
 	endwin(); 
 
 	return 0;
 }
 
-void entityControl(int pos[3], int screen[2]) { 
+void entityControl(int pos[3], int screen[2], int foodPos[2]) { 
 	int ch = getch(); 
 	while(ch != 101) {
 		usleep(500000); // sleep for .5 seconds 
@@ -74,47 +75,46 @@ void entityControl(int pos[3], int screen[2]) {
 			default:
 				break; 
 		} 
-		
-		printEntity(1, pos, screen); 
+		clear();	
+		makeMap(screen);
+		printEntity(1, pos, screen);
+		mvaddch(foodPos[0], foodPos[1], 'O');
+		refresh; 
 	}
 }
 void printEntity(int length, int pos[3], int screen[2]){ 
 	if ((pos[0] > 0)&&(pos[0] < screen[0])&&(pos[1] > 0)&&(pos[1] < screen[1])) {
-		clear();
+		//clear();
 		mvaddch(pos[0], pos[1], 'x');
-		refresh;   
+		//refresh;   
 	} 
 	 			
 }
 // Create a map
-void makeMap(){
-	system("clear");
+void makeMap(int screen[2]){
 	//create walls
-	for (int i = 0; i <= mapy+1; ++i){
-		for (int j = 0; j <= mapx +1; ++j){
-			if (i == 0 || i == mapy+1){
-				cout << "-";
+	for (int i = 0; i <= screen[0]+1; ++i){
+		for (int j = 0; j <= screen[1]+1; ++j){
+			if (i == 0 || i == screen[0]){
+				mvaddch(i,j,'-');
 			}
-			else if (i > 0 && i < mapy+1){
-				if (j == 0 || j == mapx+1){
-					cout << "|";
+			else if (i > 0 && i < screen[0]+1){
+				if (j == 0 || j == screen[1]+1){
+					mvaddch(i,j,'|');
 				}
 				else {
-					cout << " ";
+					mvaddch(i,j,' ');
 				}
 			}
 		}
-		cout << endl;
 	}
-
 }
 
-// Take input from keyboard
-void input(){
-
-}
-
-// Logic for moving
-void move(){
-
-} 
+// Creates a new food and returns its location
+int* spawnFood(int screen[2]){
+	static int foodPos[2];
+	srand(time(NULL));
+	foodPos[0] = rand() %screen[0] +1;
+	foodPos[1] = rand() %screen[1] +1;
+	return foodPos;
+};
